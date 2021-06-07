86e4a7f27845065892f8965165f5cb4732741e75
Patazone <info@patazone.co.ke>
2021-06-07 18:22:13 +1200

    june commit

--- address/models.py
+++ address/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from billing.models import BillingProfile
 
 # Create your models here.
+Address_type = (
+    ('billing', 'Billing address'),
+    ('shipping', 'Shipping address'),
+)
+
+
+class Address(models.Model):
+    billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE)
+    name = models.CharField()
