910072b354ac46328deaa64b16718d6e18d0365a
Patazone <info@patazone.co.ke>
2021-06-07 18:12:47 +1200

    june commit

---
+++ accounts/forms.py
@@ -0,0 +1,150 @@
+from django import forms
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+# from django.core.urlresolvers import reverse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+from .models import EmailActivation, GuestEmail
+
+User = get_user_model()
+
+
+class ReactivateEmailForm(forms.Form):
+    email = forms.EmailField()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = EmailActivation.objects.email_exists(email)
+        if not qs.exists():
+            register_link = reverse("register")
+            message = """Email does not exist, would you like to <a href="{link}">register</a>""".format(
+                link=register_link)
+            raise forms.ValidationError(mark_safe(message))
+        return email
+
+
+class UserAdminCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match")
+        return password2
+
+    def save(self, commit=True):
+        # save the passwords in hashed format/ encrypted format
+        user = super(UserAdminCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserDetailChangeForm(forms.ModelForm):
+    fullname = forms.CharField(label='Name', required=False, widget=forms.TextInput(attrs={"class": 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['last_name']
+
+
+class UserAdminChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+    model = User
+    fields = ('first_name', 'last_name', 'email', 'password', 'is-active', 'admin')
+
+    def clean_password(self):
+        return self.initial["password"]
+
+
+class GuestForm(forms.ModelForm):
+    class Meta:
+        model = GuestEmail
+        fields = ['email']
+
+    def __init__(self, request, *args, **kwargs):
+        self.request = request
+        super(GuestForm, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        obj = super(GuestForm, self).save(commit=False)
+        if commit:
+            obj.save()
+            request = self.request
+            request.session['guest_email_id'] = obj.id
+        return obj
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, request, *args, **kwargs):
+        self.request = request
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        request = self.request
+        data = self.cleaned_data
+        email = data.get("email")
+        password = data.get("password")
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            not_active = qs.filter(is_active=False)
+            if not_active.exists():
+                link = reverse("account:resend-activation")
+                reconfirming_msg = """Go to <a href='{resend_link}'>resend confirmation email</a>.""".format(
+                    resend_link=link)
+                confirm_email = EmailActivation.objects.filter(email=email)
+                is_confirmable = confirm_email.confirmable().exists()
+                if is_confirmable:
+                    message1 = "please check your email to confirm your account or" + reconfirming_msg.lower()
+                    raise forms.ValidationError(mark_safe(message1))
+                email_confirm_exists = EmailActivation.objects.email_exists(email).exists()
+                if email_confirm_exists:
+                    message2 = "Email not confirmed." + reconfirming_msg
+                    raise forms.ValidationError(mark_safe(message2))
+                if not is_confirmable and not email_confirm_exists:
+                    raise forms.ValidationError("This user is inactive.")
+        user = authenticate(request, username=email, password=password)
+        if user is None:
+            raise forms.ValidationError("Invalid Login Credentials")
+        login(request, user)
+        self.user = user
+        return data
+
+
+class RegisterForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        # fields = ('first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'email')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("The two passwords does not match")
+        return password2
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.is_active = False
+        # obj = EmailActivation.objects.create(user=user)
+        # obj.send_activation_email()
+        if commit:
+            user.save()
+        return user
--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
 
-# Create your views here.
+from .models import BillingProfile, Card
+
+
+def payment_Method_View(request):
+    if request.user.is_authenticated():
+        billing_profile = request.user.billingprofile
+        customer_id = billing_profile.customer_id
+
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    if not billing_profile:
+        return redirect("/cart")
+    next_url = None
+    next_ = request.GET.get('next')
+    if is_safe_url(next_, request.get_host()):
+        next_url = next_
+    context = {
+        "next_url": next_url,
+    }
+    return render(request, 'payment-methods.html', context)
+
+
+def payment_method_createView(request):
+    if request.method == "POST" and request.is_ajax():
+        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+        if not billing_profile:
+            return HttpResponse({"message": "Unable to find user"}, status_code=401)
+        token = request.POST.get("token")
+        if token is not None:
+            new_card_obj = Card.objects.add_new(billing_profile, token)
+        return JsonResponse({"message": "success ! Your card was added."})
+    return HttpResponse("error", status_code=401)
