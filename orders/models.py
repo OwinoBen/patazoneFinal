aa447a95b54243002aefcb274fbc9fa956dc4862
OwinoBen <bensonowinoopondo@gmail.com>
2021-06-24 08:15:58 +0300

    committing

--- orders/models.py
+++ orders/models.py
@@ -27,8 +27,8 @@ class UserProfile(models.Model):
     stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
     one_click_purchasing = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.user.username
+    # def __str__(self):
+    #     return self.user
 
 
 class OrderManagerQuerySet(models.query.QuerySet):
