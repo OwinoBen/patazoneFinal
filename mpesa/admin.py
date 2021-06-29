6d4036ca86fc0e85925d4f95853373ccfcfb6c8d
Patazone <info@patazone.co.ke>
2021-06-30 02:31:17 +1200

    heroku init

--- mpesa/admin.py
+++ mpesa/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from .models import Mpesa_Payments
 from .models import Post
 
-admin.site.register(Mpesa_Payments)
+
+class Mpesa(admin.ModelAdmin):
+    list_display = [
+        'MerchantRequestID',
+        'CheckoutRequestID',
+        'Amount',
+        'MpesaReceiptNumber',
+        'TransactionDate',
+        'PhoneNumber',
+        'Status',
+    ]
+    list_filter = ['MpesaReceiptNumber', 'PhoneNumber', 'Status']
+    search_fields = ['MpesaReceiptNumber', 'PhoneNumber', 'Status', 'TransactionDate']
+
+
+admin.site.register(Mpesa_Payments, Mpesa)
 admin.site.register(Post)
