7a5063899e66beb0641838c62dc981ba0b135316
Patazone <info@patazone.co.ke>
2021-07-02 19:48:11 +1200

    heroku init

--- mpesa/views.py
+++ mpesa/views.py
@@ -115,6 +115,17 @@ def lipa_na_mpesa(request):
         payment.PhoneNumber = req['Body']['stkCallback']['CallbackMetadata']['Item'][4]['Value']
         payment.save()
 
+        if payment:
+            order = Order.objects.get(ordered=False)
+            orderitems = order.cart.all()
+            orderitems.update(ordered=True)
+            for item in orderitems:
+                item.save()
+            order.ordered = True
+            order.payment = payment
+            order.save()
+        else:
+            print("order not saved try again later")
 
     except:
         pass
