f39c86c6797622a10af3e4655fbfdf7264f209f2
Patazone <info@patazone.co.ke>
2021-07-09 20:05:27 +1200

    heroku init

--- patazoneEcommerce/aws/conf.py
+++ patazoneEcommerce/aws/conf.py
@@ -10,12 +10,12 @@ with open('config.json') as config_file:
 AWS_ACCESS_KEY_ID = "AKIA364ENTIR6TTWV3BA"
 # AWS_SECRET_ACCESS_KEY = config.get('AWS_SECRET_ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+# AWS_GROUP_NAME = "patazone_eCommerce_Group"
+# AWS_USERNAME = "patazone"
 
-AWS_GROUP_NAME = "patazone_eCommerce_Group"
-AWS_USERNAME = "patazone"
-
-AWS_STORAGE_BUCKET_NAME = 'patazone'
-
+# AWS_STORAGE_BUCKET_NAME = 'patazone'
+AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
@@ -27,8 +27,7 @@ STATICFILES_STORAGE = 'patazoneEcommerce.storageLocation.utils.StaticStorage'
 # s3 public media settings
 PUBLIC_MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-# DEFAULT_FILE_STORAGE = 'patazoneEcommerce.storageLocation.utils.PublicMediaStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'patazoneEcommerce.storageLocation.utils.PublicMediaStorage'
 # s3 private media settings
 PRIVATE_MEDIA_LOCATION = 'private'
 PRIVATE_FILE_STORAGE = 'patazoneEcommerce.storageLocation.utils.PrivateMediaStorage'
