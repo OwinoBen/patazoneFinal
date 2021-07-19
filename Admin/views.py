111d8a89facea41e758a0e53da0af465456d7444
Patazone <info@patazone.co.ke>
2021-07-20 00:07:59 +1200

    heroku init

--- Admin/views.py
+++ Admin/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+
 from accounts.models import User
 
-
 # Create your views here.
+from orders.models import UserProfile
+
 
 def adminpage(request):
     return render(request, 'dashboard/dashboard.html')
@@ -14,5 +17,24 @@ def userList(request):
     return render(request, 'users/users.html', context)
 
 
+@csrf_protect
 def addUsers(request):
-    return render(request,'users/addUser.html')
+    if request.method == 'POST':
+        if request.POST['username'] != '' and request.POST['email'] != '' and request.POST['fname'] != '' and \
+                request.POST['lname'] != '':
+            try:
+                username = User.objects.get(username=request.POST['username'])
+                email = User.objects.get(email=request.POST['email'])
+                return render(request, 'users/addUser.html',
+                              {'error': "User with the given credentials already exists"})
+            except User.DoesNotExist:
+                user = User.objects.create_user(email=request.POST['email'], username=request.POST['username'],
+                                                first_name=request.POST['firstname'],
+                                                last_name=request.POST['lastname'],
+                                                password=request.POST['password'])
+                userprofile = UserProfile()
+                userprofile.user = user
+                userprofile.save()
+                return re
+
+    return render(request, 'users/addUser.html')
