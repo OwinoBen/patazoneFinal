46dafec5cf5a259a0ac05c862e7970d0793078ae
Patazone <info@patazone.co.ke>
2021-07-20 01:27:50 +1200

    heroku init

--- Admin/views.py
+++ Admin/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 
 from accounts.models import User
@@ -17,24 +18,30 @@ def userList(request):
     return render(request, 'users/users.html', context)
 
 
-@csrf_protect
 def addUsers(request):
     if request.method == 'POST':
         if request.POST['username'] != '' and request.POST['email'] != '' and request.POST['fname'] != '' and \
                 request.POST['lname'] != '':
-            try:
-                username = User.objects.get(username=request.POST['username'])
-                email = User.objects.get(email=request.POST['email'])
-                return render(request, 'users/addUser.html',
-                              {'error': "User with the given credentials already exists"})
-            except User.DoesNotExist:
-                user = User.objects.create_user(email=request.POST['email'], username=request.POST['username'],
-                                                first_name=request.POST['firstname'],
-                                                last_name=request.POST['lastname'],
-                                                password=request.POST['password'])
-                userprofile = UserProfile()
-                userprofile.user = user
-                userprofile.save()
-                return re
+            if request.POST['password'] == request.POST['confirmpass']:
+                try:
+                    user = User.objects.get(username=request.POST['username'])
+                    email = User.objects.get(email=request.POST['email'])
+                    return render(request, 'users/addUser.html',
+                                  {'error': "User with the given credentials already exists"})
+                except User.DoesNotExist:
+                    user = User.objects.create_user(email=request.POST['email'], username=request.POST['username'],
+                                                    first_name=request.POST['fname'],
+                                                    last_name=request.POST['lname'],
+                                                    password=request.POST['password'])
+                    userprofile = UserProfile()
+                    userprofile.user = user
+                    userprofile.save()
+                    messages.success(request, 'User successfully added')
+                    return redirect('Admins:user')
+            else:
+                messages.error(request, 'The two passwords do not match')
+                return render(request, 'users/addUser.html', {'error': 'he two passwords do not match'})
+        else:
+            return render(request, 'users/addUser.html', {'error': 'Please fill all the required fields'})
 
     return render(request, 'users/addUser.html')
